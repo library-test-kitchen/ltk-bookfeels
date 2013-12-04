0aa7b26bdd929c044e1a65a8dcb17e350200c80a
Paulina Haduong <phaduong@paulina-haduongs-macbook-pro.local>
2013-12-03 20:12:28 -0500

    commit swiftmailer test4

--- giftest.php
+++ giftest.php
@@ -2,21 +2,19 @@
 require_once 'lib/swift_required.php';
  
 // Create the SMTP configuration
-$transport = Swift_SmtpTransport::newInstance("smtp.audero.it", 25);
-$transport->setUsername("Username");
-$transport->setPassword("Password");
+$transport = Swift_SmtpTransport::newInstance("smtp.gmail.com", 465, 'ssl');
+$transport->setUsername("bookfeels@gmail.com");
+$transport->setPassword("librarytestkitchen");
  
 // Create the message
 $message = Swift_Message::newInstance();
 $message->setTo(array(
    "paulina.haduong@gmail.com" => "Paulina"
 ));
-$message->setCc(array("a.derosa@audero.it" => "Aurelio De Rosa"));
-$message->setBcc(array("boss@bank.com" => "Bank Boss"));
 $message->setSubject("This email is sent using Swift Mailer");
 $message->setBody("You're our best client ever.");
 $message->setFrom("account@bank.com", "Your bank");
-$message->attach(Swift_Attachment::fromPath("path/to/file/file.zip"));
+// $message->attach(Swift_Attachment::fromPath("path/to/file/file.zip"));
  
 // Send the email
 $mailer = Swift_Mailer::newInstance($transport);
